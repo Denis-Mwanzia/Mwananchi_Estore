6166d7ac19373c23333a3c54463ed0918cdc957e
Denis-Mwanzia <denismwanzia56@gmail.com>
2023-09-08 07:11:27 +0300

    created template, static files and configuration

--- Mwananchi_Estore/settings.py
+++ Mwananchi_Estore/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Mwananchi_Estore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    'Mwananchi_Estore/static',
+]
+
+STATIC_ROOT = BASE_DIR /'static'
