67603c213f509ba9578b873529bfb3b0fc37c40f
Denis-Mwanzia <denismwanzia56@gmail.com>
2023-09-08 18:11:26 +0300

    created category app

---
+++ category/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Category (models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(unique=True, max_length=100)
+    description = models.TextField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to='photo/categories', blank=True)
+
+    class Meta:
+        db_table = ''
+        managed = True
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.category_name
