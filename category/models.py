95fb0b123224b06b43a1c54fc4257cc7da8458e2
Denis-Mwanzia <denismwanzia56@gmail.com>
2023-09-15 21:46:42 +0300

    added out of stock functionality to the app

--- category/models.py
+++ category/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Category (models.Model):
     category_name = models.CharField(max_length=100, unique=True)
@@ -12,6 +12,10 @@ class Category (models.Model):
         managed = True
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-
+        
+    def get_url(self):
+        return reverse('product_by_category', args=[self.slug])
+    
+        
     def __str__(self):
         return self.category_name
