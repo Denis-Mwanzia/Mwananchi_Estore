9ee38e300e30b0fb4f7388cf758466069f0a27ca
Denis-Mwanzia <denismwanzia56@gmail.com>
2023-09-19 08:09:14 +0300

    added cart item deletion functionality

--- carts/views.py
+++ carts/views.py
@@ -45,7 +45,14 @@ def remove_cart(request, product_id):
     else:
         cart_item.delete()
     return redirect('cart')
-    
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product_id = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product_id, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
 
 def cart(request):
     total = 0
