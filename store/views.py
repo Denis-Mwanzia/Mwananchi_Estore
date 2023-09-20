b23144bd29d02e438353523442e4748ed43e7650
Denis-Mwanzia <denismwanzia56@gmail.com>
2023-09-20 06:58:23 +0300

    added in_cart function which checks whether the product is already in cart

--- store/views.py
+++ store/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+
 # Create your views here.
+
 
 def store(request, category_slug=None):
     categories = None
     products = None
 
-    if category_slug  is not None:  # Corrected condition
+    if category_slug is not None:  # Corrected condition
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
+        products = Product.objects.filter(
+            category=categories, is_available=True)
         product_count = products.count()
     else:
         products = Product.objects.filter(is_available=True)
@@ -18,10 +23,16 @@ def store(request, category_slug=None):
     context = {'products': products, 'product_count': product_count}
     return render(request, 'store/store.html', context)
 
+
 def product_detail(request, category_slug, product_slug):
     try:
-        single_product = Product.objects.get(category__slug = category_slug, slug=product_slug)
+        single_product = Product.objects.get(
+            category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(
+            request), product=single_product).exists()
     except Exception as e:
         raise e
-    context = {'single_product': single_product}
+    context = {'single_product': single_product,
+               'in_cart': in_cart,
+               }
     return render(request, 'store/product_detail.html', context)
