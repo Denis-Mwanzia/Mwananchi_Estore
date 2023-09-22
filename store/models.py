cd83e65f2729a3ea5a7fb6fa72723f63252f640b
Denis-Mwanzia <denismwanzia56@gmail.com>
2023-09-22 22:36:24 +0300

    created variation model

--- store/models.py
+++ store/models.py
@@ -3,9 +3,11 @@ from category.models import Category
 from django.urls import reverse
 
 # Create your models here.
+
+
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=100, unique= True)
+    product_name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=150, blank=True)
     price = models.IntegerField()
@@ -14,9 +16,25 @@ class Product(models.Model):
     image = models.ImageField(upload_to='photos/products')
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-    
+
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
-    
+
     def __str__(self):
         return self.product_name
+
+    variation_category_choice = (
+        ('color', 'color'),
+        ('size', 'size'),
+    )
+
+    class Variation(models.Model):
+        product = models.ForeignKey(Product, on_delete=CASCADE)
+        variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+        variation_value = models.CharField(max_length=100)
+        is_active = models.BooleanField(default=True)
+        created_date = models.DateTimeField(auto_now_add=True)
+        
+        def __str__(self):
+            return self.product
+        
