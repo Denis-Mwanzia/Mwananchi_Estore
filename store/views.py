b8ac45730dc706d3aac678302d3130a8c1d6d2e4
Denis-Mwanzia <denismwanzia56@gmail.com>
2023-09-26 10:20:15 +0300

    added search functionality

--- store/views.py
+++ store/views.py
@@ -4,16 +4,34 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
+from django.db.models import Q
 
 
 # Create your views here.
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    if keyword:
+        products = Product.objects.order_by('-created_date').filter(
+            Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+        product_count = products.count()
+        context = {
+            'products': products,
+            'product_count':product_count
+        }
+    else:
+        products = Product.objects.all()
+        context = {
+            'products': products,
+        }
+    return render(request, 'store/store.html', context)
 
 
 def store(request, category_slug=None):
     categories = None
     products = None
 
-    if category_slug is not None:  # Corrected condition
+    if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(
             category=categories, is_available=True)
