db9c3a486c9d0bf8470c958b9658d97f85328ee5
Denis-Mwanzia <denismwanzia56@gmail.com>
2023-09-26 08:00:33 +0300

    added functionality where we can get instance of variation

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from store.models import Product
+from store.models import Product, Variation
 # Create your models here.
 
 
@@ -12,7 +12,8 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE,)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation = models.ManyToManyField(Variation, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
@@ -20,5 +21,5 @@ class CartItem(models.Model):
     def sub_total(self):
         return self.product.price * self.quantity
 
-    def __str__(self):
-        return f"CartItem - Product: {self.product.product_name}, Quantity: {self.quantity}"
+    def __unicode__(self):
+        return product
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 # Create your views here.
 
@@ -12,10 +12,19 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    color = request.GET(color)
-    size = request.GET(size)
     # get the product for our cart
     product = Product.objects.get(id=product_id)
+    product_variation = []
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except Exception as e:
+                pass
+    
     try:
         # get the cart id from the session
         cart = Cart.objects.get(cart_id=_cart_id(request))
