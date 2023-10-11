eb6103bcb095acf9a7123130310c49c8f8e1801f
Denis-Mwanzia <denismwanzia56@gmail.com>
2023-10-11 07:25:53 +0300

    added a check function on whether a user has purchased a product before submitting product review

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Variation
+from .models import Product, Variation, ReviewRating
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('category', 'product_name', 'price', 'stock', 'created_date','modified_date', 'is_available',)
@@ -9,5 +9,9 @@ class VariationAdmin(admin.ModelAdmin):
     list_display = ('product','variation_category','variation_value','is_active',)
     list_editable = ('is_active',)
     list_filter = ('product','variation_category','variation_value',)
+    
+class ReviewRatingAdmin(admin.ModelAdmin):
+    list_display = ('product','user','review','sentiment','rating')
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation,VariationAdmin)
+admin.site.register(ReviewRating, ReviewRatingAdmin)
--- store/views.py
+++ store/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, ReviewRating
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from django.db.models import Q
-
+from .forms import ReviewForm
+from django.contrib import messages
+from django.shortcuts import render, redirect
+import joblib
+import random
+import re
 
 # Create your views here.
 
@@ -58,7 +63,98 @@ def product_detail(request, category_slug, product_slug):
             request), product=single_product).exists()
     except Exception as e:
         raise e
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
     context = {'single_product': single_product,
                'in_cart': in_cart,
+               'orderproduct': orderproduct,
                }
     return render(request, 'store/product_detail.html', context)
+
+
+
+# Load the SVM model and TF-IDF vectorizer
+svm_model = joblib.load('Sentiment_analysis_and_Recomender_system/sentiment_model.joblib')
+tfidf_vectorizer = joblib.load('Sentiment_analysis_and_Recomender_system/tfidf_vectorizer.joblib')
+
+# Function to preprocess the input review
+def preprocess(user_review):
+    if isinstance(user_review, str):
+        user_review = re.sub('[^a-zA-Z0-9]', ' ', user_review)
+        user_review = re.sub('\s+', ' ', user_review)
+        return user_review
+    else:
+        return str(user_review)
+
+# Function to predict sentiment based on user input
+def predict_sentiment(user_review):
+    # Preprocess the input review
+    user_review = preprocess(user_review)
+    # Vectorize the user review using the TF-IDF vectorizer
+    user_review_tfidf = tfidf_vectorizer.transform([user_review])
+    # Predict using the trained model (SVM in this case)
+    prediction = svm_model.predict(user_review_tfidf)
+    # Map predicted sentiment to labels
+    sentiment_labels = {0: 'Negative', 1: 'Neutral', 2: 'Positive'}
+    predicted_sentiment_label = sentiment_labels[prediction[0]]
+    return predicted_sentiment_label
+
+# Function to map sentiment to rating
+def map_sentiment_to_rating(sentiment):
+    if sentiment == 'Negative':
+        return random.uniform(1, 2)  # Assign a random rating between 1 and 2 for negative sentiment
+    elif sentiment == 'Neutral':
+        return 3  # Assign a fixed rating of 3 for neutral sentiment
+    elif sentiment == 'Positive':
+        return random.uniform(4, 5)  # Assign a random rating between 4 and 5 for positive sentiment
+    else:
+        return None
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+
+    if request.method == 'POST':
+        user_review = request.POST.get('review', '')  # Get the review from the form
+
+        # Predict sentiment and map to rating
+        predicted_sentiment = predict_sentiment(user_review)
+        predicted_rating = map_sentiment_to_rating(predicted_sentiment)
+
+        try:
+            # Check if the review already exists for the user and product
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+
+            # Update the existing review with the new data from the form
+            form = ReviewForm(request.POST)
+            form = ReviewForm(request.POST, instance=reviews)
+            if form.is_valid():
+                data = form.save(commit=False)
+                data.sentiment = predicted_sentiment  # Assign predicted sentiment
+                data.rating = predicted_rating 
+                form.save()
+                messages.success(request, 'Thank you! Your review has been updated.')
+                return redirect(url)
+
+        except ReviewRating.DoesNotExist:
+            # Create a new review entry
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = form.save(commit=False)
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.sentiment = predicted_sentiment  # Assign predicted sentiment
+                data.rating = predicted_rating  # Assign predicted rating
+                data.save()
+                messages.success(request, 'Thank you! Your review has been submitted.')
+                return redirect(url)
+
+    # Redirect back to the product detail page if the request method is not POST
+    return redirect(url)
+
+
+
+
+
+
